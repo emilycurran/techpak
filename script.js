a38a7e5213ee963997bc416991a5c6f3ce0d071b
emily_curran <1303currane@gmail.com>
2025-03-01 19:46:35 +0000

    forcing animation to run on mobile

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("techpack").addEventListener("click", function() {
     window.location.href = "tech_pack.pdf"; // Redirects the whole page to the PDF
 });
+
+document.addEventListener("touchstart", function() {
+    document.querySelector("img, svg").style.animation = "wiggle 0.5s infinite ease-in-out alternate";
+});
